2c56af08b66075085af02a8f1702202e12d55fb8
sergeitovpeko <sergei.tovpeko@yandex.ru>
2023-02-16 02:51:56 +0300

    Исправил ошибки 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,12 @@ const editButton = document.querySelector('.profile__button_type-edit');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
 
-let formElement = document.querySelector('.popup__form');
+let formElement = document.querySelector('.popup__form-item');
 let nameProfile = document.querySelector ('.profile__name')
 let jobProfile = document.querySelector ('.profile__job')
 
-let nameInput = popup.querySelector('.form__item_name_input');
-let jobInput = popup.querySelector('.form__item_job_input');
+let nameInput = popup.querySelector('.popup__form-item_name_input');
+let jobInput = popup.querySelector('.popup__form-item_job_input');
 
 
 // Открытие и закрытие попапа по нажатию на крестик
