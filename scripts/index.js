4d1a89180cab71031b194656972d4c48418f5392
sergeitovpeko <sergei.tovpeko@yandex.ru>
2023-02-15 21:16:36 +0300

    Исправил ошибки после 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,40 @@
-// Открытие и закрытие попапа по нажатию на крестик
+// Объявление переменных
 const editButton = document.querySelector('.profile__button_type-edit');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
 
+let formElement = document.querySelector('.popup__form');
+let nameProfile = document.querySelector ('.profile__name')
+let jobProfile = document.querySelector ('.profile__job')
+
+let nameInput = popup.querySelector('.popup__input-text_name');
+let jobInput = popup.querySelector('.popup__input-text_job');
+
+
+// Открытие и закрытие попапа по нажатию на крестик
 const handleEditButtonClick = () => {
     popup.classList.add('popup_opened');
+    nameInput.value = nameProfile.textContent.trim();
+    jobInput.value = jobProfile.textContent.trim();
 }
 
-const handleCloseButtonClick = () => {
+const closePopup = () => {
   popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', handleEditButtonClick);
-closeButton.addEventListener('click', handleCloseButtonClick);
+closeButton.addEventListener('click', closePopup);
 
 
 // Редактирование информации в профиле
-let formElement = document.querySelector('.popup__form');
-
-let nameProfile = document.querySelector ('.profile__name')
-let jobProfile = document.querySelector ('.profile__job')
-
-let nameInput = popup.querySelector('input[name="name"]');
-let jobInput = popup.querySelector('input[name="job"]');
-
 function handleFormSubmit (evt) {
   evt.preventDefault();
  
   nameProfile.textContent = nameInput.value.trim();
+  console.log();
   jobProfile.textContent = jobInput.value.trim();
 
-  handleCloseButtonClick();
+  closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
